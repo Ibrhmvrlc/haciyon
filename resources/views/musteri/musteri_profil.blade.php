885b71e6ec6dd88dd784dc9015dc5b83f67a2c0d
Ibrhmvrlc <varelci.i@gmail.com>
2024-07-27 16:56:55 +0300

    Müşteri Modülü geliştirmesi
    Müşterilerin ayrıntılarının görüneceği profil sayfaları yapılmaya devam ediliyor.

--- resources/views/musteri/musteri_profil.blade.php
+++ resources/views/musteri/musteri_profil.blade.php
@@ -199,9 +199,9 @@
                     <div class="profile-tab">
                         <div class="custom-tab-1">
                             <ul class="nav nav-tabs">
-                                <li class="nav-item"><a href="#my-posts" data-toggle="tab" class="nav-link active show">Genel</a>
+                                <li class="nav-item"><a href="#genel" data-toggle="tab" class="nav-link active show">Genel</a>
                                 </li>
-                                <li class="nav-item"><a href="#about-me" data-toggle="tab" class="nav-link">Fatura Bilgileri</a>
+                                <li class="nav-item"><a href="#faturabilgi" data-toggle="tab" class="nav-link">Fatura Bilgileri</a>
                                 </li>
                                 <li class="nav-item"><a href="#fiyat" data-toggle="tab" class="nav-link">Fiyat</a> <!-- OLUSTUR -->
                                 </li>
@@ -209,59 +209,115 @@
                                 </li>
                             </ul>
                             <div class="tab-content">
-                                <div id="my-posts" class="tab-pane fade active show">
+                                <div id="genel" class="tab-pane fade active show">
                                     <div class="my-post-content pt-3">
                                         <div class="post-input">
                                             <textarea name="textarea" id="textarea" cols="30" rows="5" class="form-control bg-transparent" placeholder="Müşteri ile ilgili not al...."></textarea>
                                             <a href="xxxxxxxxxxxxxxxxx" class="btn btn-primary">Kaydet</a> 
-                                            <button type="button" class="btn btn-info text-white" data-toggle="modal" data-target="#xxxxxxx">
-                                                Hatırlatıcı kur<span class="btn-icon-right"><i class="ti-bell"></i></span>
+                                           
+                                        </div>
+
+
+
+
+                                        <div class="profile-uoloaded-post border-bottom-1 pb-5">
+                                            <h4>
+                                                Müşteri Notları <i class="fa fa-clock-o"></i>
+                                            </h4>
+                                            <p>Notlar benzeri bir tablo oluşturulup müşteri id'sine göre döngü yapılarak notlar burada gösterilecek.</p>
+                                            
+                                            
+                                            <button type="button" class="btn btn-success text-white">
+                                                <span><i class="fa fa-check"></i></span>
                                             </button>
-                                        </div>
-                                        <div class="profile-uoloaded-post border-bottom-1 pb-5">
-                                            <img src="images/profile/8.jpg" alt="" class="img-fluid">
-                                            <a class="post-title" href="javascript:void()">
-                                                <h4>Collection of textile samples lay spread</h4>
-                                            </a>
-                                            <p>A wonderful serenity has take possession of my entire soul like these sweet morning of spare which enjoy whole heart.A wonderful serenity has take possession of my entire soul like these sweet morning
-                                                of spare which enjoy whole heart.</p>
-                                            <button class="btn btn-primary mr-3"><span class="mr-3"><i
-                                                        class="fa fa-heart"></i></span>Like</button>
-                                            <button class="btn btn-secondary"><span class="mr-3"><i
-                                                        class="fa fa-reply"></i></span>Reply</button>
-                                        </div>
-                                        <div class="profile-uoloaded-post border-bottom-1 pb-5">
-                                            <img src="images/profile/9.jpg" alt="" class="img-fluid">
-                                            <a class="post-title" href="javascript:void()">
-                                                <h4>Collection of textile samples lay spread</h4>
-                                            </a>
-                                            <p>A wonderful serenity has take possession of my entire soul like these sweet morning of spare which enjoy whole heart.A wonderful serenity has take possession of my entire soul like these sweet morning
-                                                of spare which enjoy whole heart.</p>
-                                            <button class="btn btn-primary mr-3"><span class="mr-3"><i
-                                                        class="fa fa-heart"></i></span>Like</button>
-                                            <button class="btn btn-secondary"><span class="mr-3"><i
-                                                        class="fa fa-reply"></i></span>Reply</button>
-                                        </div>
-                                        <div class="profile-uoloaded-post pb-5">
-                                            <img src="images/profile/8.jpg" alt="" class="img-fluid">
-                                            <a class="post-title" href="javascript:void()">
-                                                <h4>Collection of textile samples lay spread</h4>
-                                            </a>
-                                            <p>A wonderful serenity has take possession of my entire soul like these sweet morning of spare which enjoy whole heart.A wonderful serenity has take possession of my entire soul like these sweet morning
-                                                of spare which enjoy whole heart.</p>
-                                            <button class="btn btn-primary mr-3"><span class="mr-3"><i
-                                                        class="fa fa-heart"></i></span>Like</button>
-                                            <button class="btn btn-secondary"><span class="mr-3"><i
-                                                        class="fa fa-reply"></i></span>Reply</button>
+                                            <button type="button" class="btn btn-danger">
+                                                <span><i class="fa fa-times"></i></span>
+                                            </button>
                                         </div>
                                         <div class="text-center mb-2"><a href="javascript:void()" class="btn btn-primary">Load More</a>
                                         </div>
                                     </div>
                                 </div>
-                                <div id="about-me" class="tab-pane fade">
+                                <div id="faturabilgi" class="tab-pane fade">
                                     <div class="profile-about-me">
                                         <div class="pt-4 border-bottom-1 pb-4">
                                             <h4 class="text-primary">About Me</h4>
+                                            <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence was created for the
+                                                bliss of souls like mine.I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents.</p>
+                                            <p>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed
+                                                in a nice, gilded frame.</p>
+                                        </div>
+                                    </div>
+                                    <div class="profile-skills pt-2 border-bottom-1 pb-2">
+                                        <h4 class="text-primary mb-4">Skills</h4>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Admin</a>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Dashboard</a>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Photoshop</a>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Bootstrap</a>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Responsive</a>
+                                        <a href="javascript:void()" class="btn btn-outline-dark btn-rounded pl-4 my-3 my-sm-0 pr-4 mr-3 m-b-10">Crypto</a>
+                                    </div>
+                                    <div class="profile-lang pt-5 border-bottom-1 pb-5">
+                                        <h4 class="text-primary mb-4">Language</h4><a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
+                                                class="flag-icon flag-icon-us"></i> English</a> <a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
+                                                class="flag-icon flag-icon-fr"></i> French</a>
+                                        <a href="javascript:void()" class="text-muted pr-3 f-s-16"><i
+                                                class="flag-icon flag-icon-bd"></i> Bangla</a>
+                                    </div>
+                                    <div class="profile-personal-info">
+                                        <h4 class="text-primary mb-4">Personal Information</h4>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Name <span class="pull-right">:</span>
+                                                </h5>
+                                            </div>
+                                            <div class="col-9"><span>Mitchell C.Shay</span>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Email <span class="pull-right">:</span>
+                                                </h5>
+                                            </div>
+                                            <div class="col-9"><span>example@examplel.com</span>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Availability <span class="pull-right">:</span></h5>
+                                            </div>
+                                            <div class="col-9"><span>Full Time (Free Lancer)</span>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Age <span class="pull-right">:</span>
+                                                </h5>
+                                            </div>
+                                            <div class="col-9"><span>27</span>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Location <span class="pull-right">:</span></h5>
+                                            </div>
+                                            <div class="col-9"><span>Rosemont Avenue Melbourne,
+                                                    Florida</span>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-4">
+                                            <div class="col-3">
+                                                <h5 class="f-w-500">Year Experience <span class="pull-right">:</span></h5>
+                                            </div>
+                                            <div class="col-9"><span>07 Year Experiences</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="fiyat" class="tab-pane fade">
+                                    <div class="profile-about-me">
+                                        <div class="pt-4 border-bottom-1 pb-4">
+                                            <h4 class="text-primary">Fiyat</h4>
                                             <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence was created for the
                                                 bliss of souls like mine.I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents.</p>
                                             <p>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed
